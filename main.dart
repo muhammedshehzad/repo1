0a957d75f7fa65d6bf2dba97f1f6fce78f31455f
Muhammed Shehzad <142075133+muhammedshehzad@users.noreply.github.com>
2025-03-29 11:19:24 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:van_sale_applicatioin/provider_and_models/order_picking_provider.dart';
+import 'package:van_sale_applicatioin/provider_and_models/sales_order_provider.dart';
+import 'package:van_sale_applicatioin/secondary_pages/a.dart';
+
+import 'authentication/login.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => SalesOrderProvider()),
+        ChangeNotifierProvider(create: (context) => OrderPickingProvider()),
+      ],
+      child: MaterialApp(
+        theme: appTheme,
+        debugShowCheckedModeBanner: false,
+        home: const Login(),
+      ),
+    ),
+  );
+}
